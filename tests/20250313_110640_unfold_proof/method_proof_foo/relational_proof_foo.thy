cb0584a6e5087a3407e6c6a9aa4052f3128f8ade
Yushuo Xiao <yushuo.xiao@inf.ethz.ch>
2025-03-31 12:47:20 +0200

    extract predicate location relation into separate lemma and adjust ML tactics

--- tests/20250313_110640_unfold_proof/method_proof_foo/relational_proof_foo.thy
+++ tests/20250313_110640_unfold_proof/method_proof_foo/relational_proof_foo.thy
@@ -176,6 +176,7 @@ schematic_goal
   sorry
 
 
+
 end
 
 end
--- viper-total-heaps/PredicateRel.thy
+++ viper-total-heaps/PredicateRel.thy
@@ -763,6 +763,91 @@ next
 qed
 
 
+lemma exp_rel_predicate_loc:
+  assumes
+    CtxtFunWf: "ctxt_wf Pr TyRep F FunMap ctxt_bpl" and
+    StateRel: "state_rel Pr StateCons TyRep Tr AuxPred ctxt_bpl \<omega>def \<omega> ns" and
+    RedArgsVpr: "red_pure_exps_total ctxt_vpr (Some \<omega>def_opt) e_args_vpr \<omega> (Some v_args_vpr)" and
+    ArgsWellTy: "\<forall>pdecl. program.predicates (program_total ctxt_vpr) ''P'' = Some pdecl \<longrightarrow>
+                         vals_well_typed (absval_interp_total ctxt_vpr) v_args_vpr (ViperLang.predicate_decl.args pdecl)" and
+    FunName: "FunMap FPredicateLoc_P = ''P''" and
+    PredDecl: "program.predicates (program_total ctxt_vpr) ''P'' = Some pdecl" and
+    ArgsRestrict: "predicate_decl.args pdecl = [TRef]" and
+    "e_args_vpr = [e_arg_vpr]" and
+    ArgRel: "exp_rel_vpr_bpl (state_rel Pr StateCons TyRep Tr AuxPred ctxt_bpl) ctxt_vpr ctxt_bpl e_arg_vpr e_arg_bpl"
+  shows "red_expr_bpl ctxt_bpl (FunExp ''P'' [] [e_arg_bpl]) ns (AbsV (AField (PredSnapshotField (''P'',v_args_vpr))))"
+proof -
+  obtain v_arg_vpr where "v_args_vpr = [v_arg_vpr]"
+    by (metis RedArgsVpr \<open>e_args_vpr = _\<close> option.simps(1,3) red_pure_exps_total_singleton)
+  hence eval_arg_vpr: "ctxt_vpr, Some \<omega>def_opt \<turnstile> \<langle>e_arg_vpr;\<omega>\<rangle> [\<Down>]\<^sub>t Val v_arg_vpr"
+    using RedArgsVpr \<open>e_args_vpr = _\<close> red_exp_list_normal_elim
+    by fastforce
+  obtain v_arg_bpl where
+    "red_expr_bpl ctxt_bpl e_arg_bpl ns v_arg_bpl" and
+    arg_rel: "val_rel_vpr_bpl v_arg_vpr = v_arg_bpl"
+    using ArgRel[unfolded exp_rel_vpr_bpl_def exp_rel_vb_single_def] eval_arg_vpr StateRel
+    by blast
+
+  obtain r where "v_arg_vpr = VRef r"
+    using ArgsWellTy[THEN spec, of pdecl, THEN mp, OF PredDecl,
+        unfolded ArgsRestrict vals_well_typed_def \<open>v_args_vpr = _\<close>, simplified]
+      has_type_get_type has_type_simps(8)
+    by blast
+  from arg_rel[unfolded \<open>v_arg_vpr = _\<close>, simplified]
+  have "v_arg_bpl = AbsV (ARef r)"
+    by auto
+
+  show ?thesis
+    apply (rule RedFunOp)
+      apply (subst FunName[symmetric])
+    using CtxtFunWf
+    unfolding ctxt_wf_def fun_interp_vpr_bpl_wf_def
+      apply blast
+     apply (rule RedExpListCons)
+      apply fact
+     apply (rule RedExpListNil)
+    apply (subst \<open>v_args_vpr = _\<close>)
+    apply (subst \<open>v_arg_bpl = _\<close>)
+    apply (subst \<open>v_arg_vpr = VRef r\<close>)
+    by (simp add: lift_fun_bpl_def)
+qed
+
+
+lemma exp_rel_perm_pred_access_2:
+  assumes
+    MaskReadWf: "mask_read_wf TyRep ctxt_bpl mask_read_bpl" and
+    StateRel: "state_rel Pr StateCons TyRep Tr AuxPred ctxt_bpl \<omega>def \<omega> ns" and
+    PlocRel: "red_expr_bpl ctxt_bpl e_ploc_bpl ns (AbsV (AField (PredSnapshotField (pid,v_args_vpr))))" and
+    "mvar = mask_var Tr" and
+    "nullConst = const_repr Tr CNull" and
+    "e_bpl = mask_read_bpl (expr.Var mvar) (expr.Var nullConst) e_ploc_bpl [TConSingle ''PredicateType_P'', TPrim TBool]"
+  shows "red_expr_bpl ctxt_bpl e_bpl ns (RealV (Rep_preal (get_mp_total_full \<omega> (pid,v_args_vpr))))"
+proof -
+  from state_rel_mask_var_rel[OF StateRel] obtain mb
+    where LookupMaskVar: "lookup_var (var_context ctxt_bpl) ns (mask_var Tr) = Some (AbsV (AMask mb))" and
+          MaskRel: "mask_rel Pr (field_translation Tr) (get_mh_total_full \<omega>) (get_mp_total_full \<omega>) mb"
+    unfolding mask_var_rel_def
+    by auto
+
+  from state_rel_boogie_const_rel[OF StateRel, unfolded boogie_const_rel_def, THEN spec, of CNull, simplified]
+  have LookupNullVar: "lookup_var (var_context ctxt_bpl) ns nullConst = Some (AbsV (ARef Null))"
+    unfolding \<open>nullConst = _\<close>
+    by auto
+
+  show ?thesis
+    unfolding \<open>e_bpl = _\<close> \<open>mvar = _\<close>
+    apply (rule mask_read_wf_apply[OF MaskReadWf])
+        defer
+        apply (rule RedVar)
+        apply (rule LookupMaskVar)
+       apply (rule RedVar)
+       apply (rule LookupNullVar)
+      apply (rule PlocRel)
+     apply simp
+    by (metis MaskRel mask_rel_def)
+qed
+
+
 lemma exhale_rel_pred_acc_upd_rel:
   assumes
     StateRelIn: "\<And> \<omega> ns. R \<omega> ns \<Longrightarrow>
@@ -802,80 +887,6 @@ lemma exhale_rel_pred_acc_upd_rel:
            (BigBlock name ((Assign m_bpl m_upd_bpl) # cs) str tr, cont)
            (BigBlock name cs str tr, cont)"
   sorry
-
-
-lemma exp_rel_perm_pred_access_2:
-  assumes
-    CtxtFunWf: "ctxt_wf Pr TyRep F FunMap ctxt_bpl" and
-    MaskReadWf: "mask_read_wf TyRep ctxt_bpl mask_read_bpl" and
-    StateRel: "state_rel Pr StateCons TyRep Tr AuxPred ctxt_bpl \<omega>def \<omega> ns" and
-    RedArgsVpr: "red_pure_exps_total ctxt_vpr (Some \<omega>def_opt) e_args_vpr \<omega> (Some v_args_vpr)" and
-    PredDecl: "program.predicates (program_total ctxt_vpr) ''P'' = Some pdecl" and
-    ArgsRestrict: "predicate_decl.args pdecl = [TRef]" and
-    ArgsWellTy: "\<forall>pdecl. program.predicates (program_total ctxt_vpr) ''P'' = Some pdecl \<longrightarrow>
-                         vals_well_typed (absval_interp_total ctxt_vpr) v_args_vpr (ViperLang.predicate_decl.args pdecl)" and
-    "e_args_vpr = [e_arg_vpr]" and
-    ArgRel: "exp_rel_vpr_bpl (state_rel Pr StateCons TyRep Tr AuxPred ctxt_bpl) ctxt_vpr ctxt_bpl e_arg_vpr e_arg_bpl" and
-    "mvar = mask_var Tr" and
-    "nullConst = const_repr Tr CNull" and
-    FunName: "FunMap FPredicateLoc_P = ''P''" and
-    "F = field_translation Tr" and
-    "e_ploc_bpl = FunExp ''P'' [] [e_arg_bpl]" and
-    "e_bpl = mask_read_bpl (expr.Var mvar) (expr.Var nullConst) e_ploc_bpl [TConSingle ''PredicateType_P'', TPrim TBool]"
-  shows "red_expr_bpl ctxt_bpl e_bpl ns (RealV (Rep_preal (get_mp_total_full \<omega> (''P'',v_args_vpr))))"
-proof -
-  from state_rel_mask_var_rel[OF StateRel] obtain mb
-    where LookupMaskVar: "lookup_var (var_context ctxt_bpl) ns (mask_var Tr) = Some (AbsV (AMask mb))" and
-          MaskRel: "mask_rel Pr (field_translation Tr) (get_mh_total_full \<omega>) (get_mp_total_full \<omega>) mb"
-    unfolding mask_var_rel_def
-    by auto
-
-  from state_rel_boogie_const_rel[OF StateRel, unfolded boogie_const_rel_def, THEN spec, of CNull, simplified]
-  have LookupNullVar: "lookup_var (var_context ctxt_bpl) ns nullConst = Some (AbsV (ARef Null))"
-    unfolding \<open>nullConst = _\<close>
-    by auto
-
-  obtain v_arg_vpr where "v_args_vpr = [v_arg_vpr]"
-    by (metis RedArgsVpr \<open>e_args_vpr = _\<close> option.simps(1,3) red_pure_exps_total_singleton)
-  hence eval_arg_vpr: "ctxt_vpr, Some \<omega>def_opt \<turnstile> \<langle>e_arg_vpr;\<omega>\<rangle> [\<Down>]\<^sub>t Val v_arg_vpr"
-    using RedArgsVpr \<open>e_args_vpr = _\<close> red_exp_list_normal_elim
-    by fastforce
-  obtain v_arg_bpl where
-    "red_expr_bpl ctxt_bpl e_arg_bpl ns v_arg_bpl" and
-    arg_rel: "val_rel_vpr_bpl v_arg_vpr = v_arg_bpl"
-    using ArgRel[unfolded exp_rel_vpr_bpl_def exp_rel_vb_single_def] eval_arg_vpr StateRel
-    by blast
-
-  obtain r where "v_arg_vpr = VRef r"
-    using ArgsWellTy[THEN spec, of pdecl, THEN mp, OF PredDecl,
-        unfolded ArgsRestrict vals_well_typed_def \<open>v_args_vpr = _\<close>, simplified]
-      has_type_get_type has_type_simps(8)
-    by blast
-  from arg_rel[unfolded \<open>v_arg_vpr = _\<close>, simplified]
-  have "v_arg_bpl = AbsV (ARef r)"
-    by auto
-
-  show ?thesis
-    unfolding \<open>e_bpl = _\<close> \<open>mvar = _\<close> \<open>e_ploc_bpl = _\<close>
-    apply (rule mask_read_wf_apply[OF MaskReadWf])
-        defer
-        apply (rule RedVar)
-        apply (rule LookupMaskVar)
-       apply (rule RedVar)
-       apply (rule LookupNullVar)
-      apply (rule RedFunOp)
-        apply (subst FunName[symmetric])
-    using CtxtFunWf
-    unfolding ctxt_wf_def fun_interp_vpr_bpl_wf_def
-        apply blast
-       apply (rule RedExpListCons)
-        apply fact
-       apply (rule RedExpListNil)
-    unfolding \<open>v_arg_bpl = _\<close>
-      apply (simp add: lift_fun_bpl_def)
-     apply simp
-    by (metis MaskRel \<open>v_arg_vpr = VRef r\<close> \<open>v_args_vpr = [v_arg_vpr]\<close> mask_rel_def)
-qed
 
 
 
